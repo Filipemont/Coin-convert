16d77c7001ce2ef75b54230921993b16de6da569
Filipemont <filipemonteiro73@gmail.com>
2021-04-13 20:25:02 -0300

    Project done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Coin-convert
+# Imersão Dev aula01
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/FilipeMont/pen/BapapNM](https://codepen.io/FilipeMont/pen/BapapNM).
+
+
---
+++ src/script.js
@@ -0,0 +1,14 @@
+var valorASerConvertido = prompt("Quanto você gatou em reais?")
+
+valorASerConvertido = parseFloat(valorASerConvertido)
+valorASerConvertido = valorASerConvertido/5.51
+valorASerConvertido = valorASerConvertido.toFixed(2)
+
+alert(valorASerConvertido)
+
+//Revisão
+//var
+//prompt = abre uma caixa com texto para resposta
+//alerta = joga na tela
+//parseint e parsefloat(formatam texto em número)
+// comando de comentario //
